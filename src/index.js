3bc8d2a448e7b4df3791729c0e9d7c315f69efb4
djangamane <janga.bussaja@gmail.com>
2025-05-23 22:05:45 -0700

    Remove Vercel Analytics to fix build issues

--- src/index.js
+++ src/index.js
@@ -3,13 +3,11 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Analytics } from '@vercel/analytics/react';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>
 );
 
